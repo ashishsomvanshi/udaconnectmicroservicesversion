bcee0b1f60cc726c0a3983c80f3f8ad45fa93a1f
Ashish Somvanshi <ashishsomvanshi@outlook.com>
2021-11-16 06:44:41 +0530

    Intitial commit

---
+++ modules/locationEventService/grpcclient.py
@@ -0,0 +1,26 @@
+import grpc
+import location_event_pb2_grpc as pb2_grpc
+import location_event_pb2 as pb2
+
+print("Sending Location")
+
+channel = grpc.insecure_channel("localhost:5005")
+stub = pb2_grpc.ItemServiceStub(channel)
+
+user_location_1 = pb2.EventLocationMessage(
+    userId=500,
+    latitude=-600,
+    longitude=70
+)
+
+user_location_2 = pb2.EventLocationMessage(
+    userId=300,
+    latitude=-200,
+    longitude=40
+)
+
+response_1 = stub.Create(user_location_1)
+response_2 = stub.Create(user_location_2)
+
+print("Sent User Locations")
+print(user_location_1, user_location_2)
---
+++ modules/locationEventService/grpcserver.py
@@ -0,0 +1,42 @@
+
+import grpc
+from concurrent import futures
+import time
+import json
+import os
+from kafka import KafkaProducer
+import location_event_pb2_grpc as pb2_grpc
+import location_event_pb2 as pb2
+
+kafka_url = os.environ["KAFKA_URL"]
+kafka_topic = os.environ["KAFKA_TOPIC"]
+
+producer = KafkaProducer(bootstrap_servers=kafka_url)
+
+class EventLocationService(pb2_grpc.EventLocationServicer):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def GetServerResponse(self, request, context):
+
+        request_value = {
+            'userId': int(request.userId),
+            'latitude': int(request.latitude),
+            'longitude': int(request.longitude)
+        }
+        user_encode_data = json.dumps(request_value, indent=2).encode('utf-8')
+        producer.send(kafka_topic, user_encode_data)
+        return pb2.EventLocationMessage(**request_value)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    pb2_grpc.add_EventLocationService_to_server(EventLocationService(), server)
+    server.add_insecure_port('[::]:5005')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
